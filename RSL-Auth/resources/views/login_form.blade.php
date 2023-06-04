ab04d9425e08cd2a5a445cfc567bb98230962462
CollinGomillia <collinlgomillia@gmail.com>
2023-06-04 12:18:05 -0400

    first commit

---
+++ RSL-Auth/resources/views/login_form.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Form</title>
+
+    <!-- css file link -->
+    <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+
+<div class="form-container">
+
+   <form action="" method="post">
+      <h3>Login Now</h3>
+      <input type="email" name="email" required placeholder="enter email address">
+      <input type="password" name="password" required placeholder="enter password">
+      <input type="submit" name=""value="login now" class="form-btn">
+      <p>don't have an account? <a href="register_form.php">register now</a></p>
+   </form>
+
+</div>
+
+</body>
+</html>
