bd6fb5b8a7dd0f3236c6504d45ed21e46a93482d
CollinGomillia <collinlgomillia@gmail.com>
2023-06-18 11:25:12 -0400

    Third commit

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -12,23 +12,89 @@ class UserController extends Controller
     }
 
     public function Store(){
+        //validate
+        $required = array(
+            'name'       => 'required',
+            'email'      => 'required|email',
+            'password'   => 'required|password'
+        );
+        $validator = Validator::make(Input::all(), $required);
 
+        // process the login
+        if ($validator->fails()) {
+            return Redirect::to('sharks/create')
+                ->withErrors($validator)
+                ->withInput(Input::except('password'));
+        } else {
+            // store
+            $user = new user;
+            $user->name       = Input::get('name');
+            $user->email      = Input::get('email');
+            $user->password = Input::get('password');
+            $user->save();
+
+            // redirect
+            Session::flash('message', 'Successfully created account!');
+            return Redirect::to('user_page');
+        }
     }
 
-    public function Index(){
+    
 
+    public function Index(){
+         // get the user
+         $user = user::find($id);
+
+         // show the view and pass the user to it
+         return View::make('user_page')
+             ->with('user', $user);
     }
 
     public function Edit($id){
+      // get the user
+      $user = user::find($id);
 
+      // show the edit form and pass the user
+      return View::make('user.edit')
+          ->with('user', $user);
     }
 
     public function Update($id){
+        $required = array(
+            'name'       => 'required',
+            'email'      => 'required|email',
+            'password' => 'required|password'
+        );
+        $validator = Validator::make(Input::all(), $required);
+
+        // process the login
+        if ($validator->fails()) {
+            return Redirect::to(' ' . $id . '/edit')
+                ->withErrors($validator)
+                ->withInput(Input::except('password'));
+        } else {
+            // store
+            $user = shark::find($id);
+            $user->name       = Input::get('name');
+            $user->email      = Input::get('email');
+            $user->password = Input::get('password');
+            $user->save();
+
+            // redirect
+            Session::flash('message', 'Successfully updated account!');
+            return Redirect::to('users');
+        }
 
     }
 
     public function Delete($id){
-        
+        // delete
+        $user = user::find($id);
+        $user->delete();
+
+        // redirect
+        Session::flash('message', 'Successfully deleted the account!');
+        return Redirect::to('users');
     }
 
 
